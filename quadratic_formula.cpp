dd13723c721c8d3b5534f49abc62da7809f641cf
tmccormack165 <60988784+tmccormack165@users.noreply.github.com>
2023-02-25 21:15:00 +0100

    separated one root solutions into their own conditional statement

--- quadratic_formula.cpp
+++ quadratic_formula.cpp
@@ -4,128 +4,91 @@
 
 using namespace std;
 
+void solve(float a, float b, float c);
 
-float * get_input(){
-    const int n = 3;
-    float a;
-    float b;
-    float c;
-
+int main() {
+    float a, b, c, roots;
+    
+    
+    //Get the coefficients from the user
     cout << "a: ";
     cin >> a;
-
     cout << "b: ";
     cin >> b;
-
     cout << "c: ";
     cin >> c;
-
-    static float arr[n] = {a, b, c};
-    return arr;
-
-}
-
-float solve_numerator(){
-    float numerator;
-    numerator = -1.0;
-    return numerator;
-
-}
-
-
-
-float solve(float * user_input){
     
+    solve(a, b, c);
 
-    float a;
-    float b;
-    float c;
+    return 0;
+}
 
-    a = user_input[0];
-    b = user_input[1];
-    c = user_input[2];
+bool check_zero(float radicand){
+    float threshold;
+    bool is_zero;
 
-    //cout << a << endl;
-    //cout << b << endl;
-    //cout << c << endl;
+    is_zero = false;
+    threshold = 0.001;
+    if(radicand <= 0 + threshold && radicand >= 0 - threshold){
+        is_zero = true;
+    }
 
-    float b_sq;
+    return is_zero;
+}
+
+
+
+void solve(float a, float b, float c){
+    float b_sq, radicand, radical, minus_b, numerator_v1, numerator_v2, denominator, root1, root2;
+    bool radicand_zero;
+
+
+    // calculate the radicand to determine the number of roots
     b_sq = pow(b, 2);
-    cout << "b^2: " << b_sq << endl;
-
-    float radicand;
     radicand = b_sq - 4 * a * c;
-
-    cout << "RADICAND: " << radicand << endl;
+    radicand_zero = check_zero(radicand);
 
     if(radicand < 0){
         cout << "ROOTS DO NOT EXIST" << endl;
-        return 0;
+    }
+    else if(radicand_zero == true){
+        // the radicand is exactly zero, so there will be one root
+        radical = sqrt(radicand);
+
+        // take the negative of b
+        minus_b = -1 * b;
+
+        numerator_v1 = minus_b + radical;
+
+        // calculate the denominator
+        denominator = 2 * a;
+
+        // calculate the roots
+        root1 = numerator_v1 / denominator;
+        cout << "root 1: " << root1 << endl;
+
     }
     else{
 
-        float radical;
         radical = sqrt(radicand);
 
-        float minus_b;
+        // take the negative of b
         minus_b = -1 * b;
 
-
-        float numerator_v1;
+        // calculate both versions of the numerator
         numerator_v1 = minus_b + radical;
-
-        float numerator_v2;
         numerator_v2 = minus_b - radical;
 
-        float denominator;
+        // calculate the denominator
         denominator = 2 * a;
 
-
-        float root1, root2;
+        // calculate the roots
         root1 = numerator_v1 / denominator;
         root2 = numerator_v2 / denominator;
 
-        cout << "ROOTS DO EXIST" << endl;
+        // print out the roots
         cout << "root 1: " << root1 << endl;
         cout << "root 2: " << root2 << endl;
     }
-    //float denominator;
-    //denominator = 2 * a;
-
-
-    float r;
-    r = 1.3;
-    return r;
-}
-
-int main() {
-    //float *user_input;
-    float r;
     
-    //user_input = get_input();
-    static float user_input[3] = {1.0, 150.0, 13.0};
-    r = solve(user_input);
-    
-    
-    /*
-    float *p;
-    float a;
-    float b;
-    float c;
-    float r;
-
-    p = get_input();
-    a = *p;
-    b = *p + 1;
-    c = *p + 2;
-    cout << "-------------------" << endl;
-    cout << "a: " << a << endl;
-    cout << "b: " << b << endl;
-    cout << "c: " << c << endl;
-    return 0;
-    r = get_x(a, b, c);
-
-    cout << r << endl;
-    */
-    return 0;
 }
